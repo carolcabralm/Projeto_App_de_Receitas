887189d797cfc8d6546225e7c3fe4eb95d36573e
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-03-30 12:15:38 -0300

    create: Telas de explorar comida e bebida

--- src/pages/FoodPages/ExploreFoods.js
+++ src/pages/FoodPages/ExploreFoods.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -6,7 +7,15 @@ function ExploreFoods() {
   return (
     <div>
       <Header value="Explore Foods" img="false" />
-      <h1>ExploreFoods</h1>
+      <Link to="/explore/foods/ingredients">
+        <button data-testid="explore-by-ingredient" type="button">By Ingredient</button>
+      </Link>
+      <Link to="/explore/foods/nationalities">
+        <button data-testid="explore-by-nationality" type="button">By Nationality</button>
+      </Link>
+      <Link to="/foods/52771">
+        <button data-testid="explore-surprise" type="button">Surprise me!</button>
+      </Link>
       <Footer />
     </div>
   );
