9145e96a3e725727c52bd078591e1f7ee2bbec95
rafaelPermec <geekchef.dev@gmail.com>
2022-03-31 15:29:21 -0300

    :construction: feat: adicionando primeiro HTML à página

--- src/pages/FoodPages/FoodInProgress.js
+++ src/pages/FoodPages/FoodInProgress.js
@@ -3,7 +3,61 @@ import React from 'react';
 function FoodInProgress() {
   return (
     <div>
-      <h1>food-se</h1>
+      <img
+        // Mudar src e alt
+        src="https://conteudo.imguol.com.br/c/entretenimento/77/2021/08/27/alimentacao-saudavel-alimentacao-equlibrada-pizza-salada-fast-food-1630077159002_v2_450x337.jpg"
+        alt="rango detalhado"
+        data-testid="recipe-photo"
+      />
+      <h1
+        data-testid="recipe-title"
+      >
+        Titulo Provisorio
+      </h1>
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Share
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favorite
+      </button>
+      <p
+        data-testid="recipe-category"
+      >
+        Categoria: Italiana
+      </p>
+      <ul>
+        <li
+          data-testid={ `${index}-ingredient-step` }
+        >
+          limão
+        </li>
+        <li>banana</li>
+        <li>alcachofra</li>
+        <li>cereja</li>
+        <li>mirtillo</li>
+      </ul>
+      <p
+        data-testid="instructions"
+      >
+        Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
+        IpsumLorem IpsumLorem IpsumLorem
+        IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
+        IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
+        IpsumLorem
+        IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+      </p>
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+      >
+        Favorite
+      </button>
     </div>
   );
 }
