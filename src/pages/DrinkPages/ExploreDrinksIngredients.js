5b32563bcc55b6a7eeec17845d20c95606b75189
carolcabralm <carolcabralm@msn.com>
2022-04-04 17:26:25 -0300

    Create: Telas explorar ingredientes - food e drink

--- src/pages/DrinkPages/ExploreDrinksIngredients.js
+++ src/pages/DrinkPages/ExploreDrinksIngredients.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -7,10 +7,41 @@ import { dataIsFood } from '../../redux/reducers/dataReducer';
 function ExploreDrinksIngredients() {
   const dispatch = useDispatch();
   dispatch(dataIsFood(false));
+
+  const number = 12;
+  const [ingredients, setIngredients] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const INGREDIENTS = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+      const response = await fetch(INGREDIENTS);
+      const data = await response.json();
+      const { drinks } = data;
+      console.log('DRINKS', drinks);
+      setIngredients(drinks);
+    })();
+  }, []);
+
   return (
     <div>
       <Header value="Explore Ingredients" img="false" />
-      <h1>ExploreDrinksIngredients</h1>
+      {
+        ingredients.map((item, index) => (
+          index < number
+          && (
+            <div
+              key={ item.strIngredient1 }
+              data-testid={ `${index}-ingredient-card` }
+            >
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ `https://www.thecocktaildb.com/images/ingredients/${item.strIngredient1}-Small.png` }
+                alt={ item.strIngredient1 }
+              />
+              <p data-testid={ `${index}-card-name` }>{ item.strIngredient1 }</p>
+            </div>
+          )
+        ))
+      }
       <Footer />
     </div>
   );
--- src/pages/FoodPages/ExploreFoodsIngredients.js
+++ src/pages/FoodPages/ExploreFoodsIngredients.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function ExploreFoodsIngredients() {
+  const number = 12;
+  const [ingredients, setIngredients] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const INGREDIENTS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+      const response = await fetch(INGREDIENTS);
+      const data = await response.json();
+      const { meals } = data;
+      console.log('MEALS', meals);
+      setIngredients(meals);
+    })();
+  }, []);
+
   return (
     <div>
       <Header value="Explore Ingredients" img="false" />
-      <h1>ExploreFoodsIngredients</h1>
+      {
+        ingredients.map((item, index) => (
+          index < number
+          && (
+            <div
+              key={ item.idIngredient }
+              data-testid={ `${index}-ingredient-card` }
+            >
+              <img data-testid={ `${index}-card-img` } src={ `https://www.themealdb.com/images/ingredients/${item.strIngredient}-Small.png` } alt={ item.strIngredient } />
+              <p data-testid={ `${index}-card-name` }>{ item.strIngredient }</p>
+            </div>
+          )
+        ))
+      }
       <Footer />
     </div>
   );
